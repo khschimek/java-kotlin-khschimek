61b3e9f0fac051b7ea1c42525a0a79f267a34f13
khschimek <khschimek@mavs.coloradomesa.edu>
2023-02-19 18:07:07 -0700

    class

--- hatObject/object.java
+++ hatObject/object.java
@@ -1,24 +1,14 @@
 private class Kettle implements AutoCloseable {
-    private boolean isFull = false;
     private int water = 0;
     private String material;
     Kettle(String material) {
         this.material=material;
     }
     public void fill() {
-        if(this.water<5) {
-            this.water += 1;
-            if(this.water==5) {
-                this.isFull=true;
-            }
-        }
+        this.water+=1;
     }
     public void empty() {
         this.water = 0;
-        this.isFull = false;
-    }
-    public boolean isFull() {
-        return this.isFull;
     }
     public int waterAmount() {
         return this.water;
